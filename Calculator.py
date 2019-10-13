fc29e4c90fc118899dfe845ac9ca7ccf0803b129
Komal2511 <43901687+Komal2511@users.noreply.github.com>
2019-10-13 09:13:51 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -8,12 +8,15 @@ def multiply(x, y):
    return x * y
 def divide(x, y):
    return x / y
+def modulus(x, y):
+   return x % y
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
-choice = input("Enter choice(1/2/3/4):")
+print("5.Modulus")
+choice = input("Enter choice(1/2/3/4/5):")
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 if choice == '1':
@@ -24,5 +27,7 @@ elif choice == '3':
    print(num1,"*",num2,"=", multiply(num1,num2))
 elif choice == '4':
    print(num1,"/",num2,"=", divide(num1,num2))
+elif choice == '5':
+   print(num1,"%",num2, "=", modulud(num1,num2))
 else:
    print("Input Unacceptable!")
